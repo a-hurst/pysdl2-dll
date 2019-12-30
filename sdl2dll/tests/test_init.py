d405c2d05681c8b3b365c06903d24c710ab1f7bd
Austin Hurst <mynameisaustinhurst@gmail.com>
2019-12-30 17:46:09 -0500

    Attempt to improve/fix tests

---
+++ sdl2dll/tests/test_init.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+def test_sdl2():
+    import sdl2
+
+def test_sdl2mixer():
+    import sdl2.sdlmixer
+
+def test_sdl2ttf():
+    import sdl2.sdlttf
+
+def test_sdl2image():
+    import sdl2.sdlimage
+
+def test_sdl2gfx():
+    import sdl2.sdlgfx
+
+def test_sdl2init():
+    import sdl2.ext
+    sdl2.ext.init()
--- test.py
+++
@@ -1,8 +0,0 @@
-import sdl2
-import sdl2.ext
-import sdl2.sdlmixer
-import sdl2.sdlttf
-import sdl2.sdlimage
-import sdl2.sdlgfx
-
-sdl2.ext.init()
