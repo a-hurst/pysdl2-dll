5934cfcc07cc67f986678b9ad8b601aeaf8b4a4f
Austin Hurst <mynameisaustinhurst@gmail.com>
2021-05-15 21:27:17 -0300

    Fix script permissions, actually fix utils

--- getdlls.py
+++ getdlls.py
@@ -11,8 +11,6 @@ try:
 except ImportError:
     from urllib2 import urlopen # Python 2
 
-from dllutils import find_symlinks, make_install_lib
-
 
 libraries = ['SDL2', 'SDL2_mixer', 'SDL2_ttf', 'SDL2_image', 'SDL2_gfx']
 
@@ -181,6 +179,52 @@ def buildDLLs(libraries, basedir, libdir):
             print('\n======= {0} {1} built sucessfully =======\n'.format(lib, libversion))
             os.chdir(basedir)
 
+
+
+# Helper functions for facilitating the compiling and/or bundling of binaries
+
+def find_symlinks(path, names):
+    """'ignore' filter for shutil.copytree that identifies whether files are
+    symlinks or not. For excluding symlinks when copying .frameworks, since
+    they're not needed for pysdl2 and Python wheels don't support them.
+    """
+    links = []
+    for f in os.listdir(path):
+        filepath = os.path.join(path, f)
+        if os.path.islink(filepath):
+            links.append(f)
+        # Some frameworks have useless duplicates instead of symlinks, so ignore those too
+        elif '.framework' in os.path.basename(path) and f != 'Versions':
+            links.append(f)
+        elif os.path.basename(path) == 'Versions' and f != 'A':
+            links.append(f)
+
+    return links
+
+
+def make_install_lib(src_path, prefix, buildenv, extra_args=None):
+    """Builds and installs a library into a given prefix using GNU Make.
+    """
+    orig_path = os.getcwd()
+    os.chdir(src_path)
+    success = True
+
+    buildcmds = [
+        ['./configure', '--prefix={0}'.format(prefix)],
+        ['make'],
+        ['make', 'install']
+    ]
+    for cmd in buildcmds:
+        if cmd[0] == './configure' and extra_args:
+            cmd = cmd + extra_args
+        p = sub.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, env=buildenv)
+        p.communicate()
+        if p.returncode != 0:
+            success = False
+            break
+
+    os.chdir(orig_path)
+    return success
 
 
 if __name__ == '__main__':
