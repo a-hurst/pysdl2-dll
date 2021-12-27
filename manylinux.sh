7daea93468c7c3d4c567cb809def66fcd4434cc5
Austin Hurst <mynameisaustinhurst@gmail.com>
2021-12-27 13:11:21 -0500

    Remove Wayland for manylinux2014, add libffi

--- manylinux.sh
+++ manylinux.sh
@@ -32,20 +32,10 @@ if command -v yum &> /dev/null; then
     # Install input libraries
     yum install -y dbus-devel libudev-devel libusb-devel ibus-devel
 
-    # Install Wayland from source
-    export WAYLAND_VERSION=1.20.0
-    export WAYLAND_URL=https://gitlab.freedesktop.org/wayland/wayland/-/archive
-    python3.7 -m pip install meson ninja
-    curl $WAYLAND_URL/$WAYLAND_VERSION/wayland-$WAYLAND_VERSION.tar.gz | tar -xz
-    cd wayland-$WAYLAND_VERSION
-    meson build/
-    ninja -C build/ install
-    cd ..
-
 else
     # For manylinux_2_24 and later (based on Debian)
     apt-get update
-    apt-get install -y libtool libdbus-1-dev
+    apt-get install -y libtool libdbus-1-dev libffi-dev
 
     # Install Pipewire from source (done before other audio backends to minimize build time)
     export PIPEWIRE_VERSION=0.3.33
