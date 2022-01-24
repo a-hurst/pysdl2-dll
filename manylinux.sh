0c1bb028ecd855f762badc14013b0d40471302d3
Austin Hurst <mynameisaustinhurst@gmail.com>
2022-01-24 16:23:55 -0400

    Remove libudev support from manylinux wheels (#11)
    * Fix bash error
    
    * Try disabling udev support to fix controllers
    
    * Retry disabling udev
    
    * And again...

--- manylinux.sh
+++ manylinux.sh
@@ -67,7 +67,7 @@ fi
 
 # If this is a tagged release, set env to strip the debug symbols from the binaries
 
-if [ -z "$CIRRUS_TAG" ]; then
+if [ -z ${CIRRUS_TAG+x} ]; then
     export SDL2DLL_RELEASE=1
 fi
 
